a4f157149320f1f947ecf945eb28ca3e3312dce0
SharpodsJuanManuel <sharpodsdatabase@gmail.com>
2024-08-07 12:07:07 -0500

    Funcionan las rutas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const UsedEmail = require("./models/NewUserDiamond"); // Importa el modelo
 const UsedEmailCarbon = require("./models/NewUserCarbon"); // Importa el modelo
+const UsedEmailGriko = require('./models/NewUserGriko')
 
 const app = express();
 const port = process.env.PORT || 5000; // Asegúrate de que 'PORT' esté en mayúsculas
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000; // Asegúrate de que 'PORT' esté en may�
 app.use(bodyParser.json());
 
 // Conecta a MongoDB
-mongoose.connect("mongodb+srv://EstivenPosada:SharpodsDataBase123.@cluster0.6ys5t.mongodb.net/",);
+mongoose.connect("mongodb+srv://JuanManuel:SharpodsDataBase123.@cluster0.6ys5t.mongodb.net/",);
 
 mongoose.connection.on("connected", () => {
   console.log("Conectado a MongoDB");
@@ -96,7 +97,7 @@ app.post("/updateEmailCarbon", async (req, res) => {
 });
 
 //Ruta para el webhook de diamond usuario desactivado
-app.post("/deactivateEmailCarbon", async (req, res) => {
+app.post("/desactivateEmailCarbon", async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
@@ -134,12 +135,86 @@ app.post("/webhookAntares", async (req, res) => {
   res.status(200).send("WebhookAntares recibido");
 });
 
-app.post("/webhookGriko", async (req, res) => {
-  // Lógica para webhookGriko
-  res.status(200).send("WebhookGriko recibido");
+app.post("/webhookGriko", async (req, res) => { //Crea Usuario
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).send("Email es requerido");
+  }
+
+  try {
+    await UsedEmailGriko.findOneAndUpdate(
+      { email },
+      { email, isActive: true },
+      { upsert: true, new: true }
+    );
+    console.log(`Email ${email} ha sido insertado/actualizado.`);
+
+    res.status(200).send("Email guardado exitosamente");
+  } catch (error) {
+    console.error(`Error guardando el email ${email}:`, error);
+    res.status(500).send("Error guardando el email");
+  }
+
+
+
 });
 
-app.post("/deactivateEmail", async (req, res) => {
+app.post("/desactivateEmailGriko", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).send("Email es requerido");
+  }
+
+  try {
+    const updatedEmail = await UsedEmailGriko.findOneAndUpdate(
+      { email },
+      { isActive: false },
+      { new: true }
+    );
+
+    if (updatedEmail) {
+      console.log(`Email ${email} ha sido desactivado.`);
+      res.status(200).send("Email desactivado exitosamente");
+    } else {
+      console.log(`Email ${email} no encontrado.`);
+      res.status(404).send("Email no encontrado");
+    }
+  } catch (error) {
+    console.error(`Error desactivando el email ${email}:`, error);
+    res.status(500).send("Error desactivando el email");
+  }
+});
+
+app.post("/updateEmailGriko", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).send("Email es requerido");
+  }
+
+  try {
+    const updatedEmail = await UsedEmailGriko.findOneAndUpdate(
+      { email },
+      { isActive: true },
+      { new: true }
+    );
+
+    if (updatedEmail) {
+      console.log(`Email ${email} ha sido actualizado.`);
+      res.status(200).send("Email actualizado exitosamente");
+    } else {
+      console.log(`Email ${email} no encontrado.`);
+      res.status(404).send("Email no encontrado");
+    }
+  } catch (error) {
+    console.error(`Error actualizando el email ${email}:`, error);
+    res.status(500).send("Error actualizando el email");
+  }
+});
+
+app.post("/desactivateEmailDiamond", async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
@@ -166,7 +241,7 @@ app.post("/deactivateEmail", async (req, res) => {
   }
 });
 
-app.post("/updateEmail", async (req, res) => {
+app.post("/updateEmailDiamond", async (req, res) => {
     const { email } = req.body;
   
     if (!email) {
