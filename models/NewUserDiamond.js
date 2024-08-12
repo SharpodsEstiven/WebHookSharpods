b2a31467087c7d079ed9db83d80f65e1ad372c6f
SharpodsTech <sharpodsofficial@gmail.com>
2024-08-12 18:47:46 -0500

    modelo userDiamond

--- models/NewUserDiamond.js
+++ models/NewUserDiamond.js
@@ -10,6 +10,10 @@ const UsedEmailSchema = new mongoose.Schema({
     type: Boolean,
     default: true,
   },
+  telegramId:{
+    type: String,
+    unique: true
+  }
 });
 
 const UsedEmailDiamond = mongoose.model('diamondUsers', UsedEmailSchema);
