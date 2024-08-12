2cd63682eae17eece6b77ab7ca78cc52a6daca8d
SharpodsEstiven <estivensharpods@gmail.com>
2024-08-12 10:36:38 -0500

    ruta de enigmario creada sin hacer prueba

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const UsedEmailCentauri = require("./models/NewUserCentauri"); // Importa el mod
 const UsedEmailCarbon = require("./models/NewUserCarbon"); // Importa el modelo
 const UsedEmailGriko = require('./models/NewUserGriko')
 const UsedEmailAntares = require("./models/NewUserAntares");
+const UsedEmailEnigmario = require("./models/newUserEnigmario");
 
 const app = express();
 const port = process.env.PORT || 5000; // Asegúrate de que 'PORT' esté en mayúsculas
@@ -24,6 +25,8 @@ mongoose.connection.on("error", (err) => {
     console.log("Error conectando a MongoDB:", err);
 });
 
+
+/* CARBON */
 // Ruta para el webhook de Carbon usuario nuevo
 app.post("/webhookCarbon", async (req, res) => {
     const { email } = req.body;
@@ -102,6 +105,8 @@ app.post("/desactivateEmailCarbon", async (req, res) => {
     }
 });
 
+
+/* ANTARES */
 // Ruta para el webhook de Antares usuario nuevo
 app.post("/webhookAntares", async (req, res) => {
     const { email } = req.body;
@@ -124,33 +129,7 @@ app.post("/webhookAntares", async (req, res) => {
     }
 });
 
-app.post("desactivateEmailAntares", async (req, res) => {
-    const { email } = req.body;
-
-    if (!email) {
-        return res.status(400).send("Email es requerido");
-    }
-
-    try {
-        const updatedEmail = await UsedEmailAntares.findOneAndUpdate(
-            { email },
-            { isActive: false },
-            { new: true }
-        );
-
-        if (updatedEmail) {
-            console.log(`Email ${email} ha sido desactivado.`);
-            res.status(200).send("Email desactivado exitosamente");
-        } else {
-            console.log(`Email ${email} no encontrado.`);
-            res.status(404).send("Email no encontrado");
-        }
-    } catch (error) {
-        console.error(`Error desactivando el email ${email}:`, error);
-        res.status(500).send("Error desactivando el email");
-    }
-})
-
+// Ruta para el webhook de antares usuario reactivado
 app.post("/updateEmailAntares", async (req, res) => {
     const { email } = req.body;
 
@@ -178,6 +157,36 @@ app.post("/updateEmailAntares", async (req, res) => {
     }
 });
 
+// Ruta para el webhook de Antares usuario desactivaddo
+app.post("desactivateEmailAntares", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send("Email es requerido");
+    }
+
+    try {
+        const updatedEmail = await UsedEmailAntares.findOneAndUpdate(
+            { email },
+            { isActive: false },
+            { new: true }
+        );
+
+        if (updatedEmail) {
+            console.log(`Email ${email} ha sido desactivado.`);
+            res.status(200).send("Email desactivado exitosamente");
+        } else {
+            console.log(`Email ${email} no encontrado.`);
+            res.status(404).send("Email no encontrado");
+        }
+    } catch (error) {
+        console.error(`Error desactivando el email ${email}:`, error);
+        res.status(500).send("Error desactivando el email");
+    }
+})
+
+
+/* GRIKO */
 // Ruta para el webhook de Griko usuario nuevo
 app.post("/webhookGriko", async (req, res) => {
     const { email } = req.body;
@@ -229,7 +238,6 @@ app.post("/desactivateEmailGriko", async (req, res) => {
 });
 
 // Ruta para el webhook de Griko usuario reactivado
-
 app.post("/updateEmailGriko", async (req, res) => {
     const { email } = req.body;
 
@@ -257,6 +265,8 @@ app.post("/updateEmailGriko", async (req, res) => {
     }
 });
 
+
+/* CENTAURI */
 // Ruta para el webhook de Centauri usuario nuevo
 app.post("/webhookCentauri", async (req, res) => {
     const { email } = req.body;
@@ -335,6 +345,8 @@ app.post("/updateEmailCentauri", async (req, res) => {
     }
 });
 
+
+/* DIAMOND */
 // Ruta para el webhook de Diamond usuario nuevo
 app.post("/webhookDiamond", async (req, res) => {
     const { email } = req.body;
@@ -412,6 +424,88 @@ app.post("/updateEmailDiamond", async (req, res) => {
         res.status(500).send("Error actualizando el email");
     }
 });
+
+
+/* ENIGMARIO */
+// Ruta para el webhook de Enigmario usuario nuevo
+app.post("/webhookEnigmario", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send("Email es requerido");
+    }
+
+    try {
+        await UsedEmail.findOneAndUpdate(
+            { email },
+            { email, isActive: true },
+            { upsert: true, new: true }
+        );
+        console.log(`Email ${email} ha sido insertado/actualizado.`);
+        res.status(200).send("Email guardado exitosamente");
+    } catch (error) {
+        console.error(`Error guardando el email ${email}:`, error);
+        res.status(500).send("Error guardando el email");
+    }
+});
+
+// Ruta para el webhook de Enigmario usuario desactivado
+app.post("/desactivateEmailEnigmario", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send("Email es requerido");
+    }
+
+    try {
+        const updatedEmail = await UsedEmailEnigmario.findOneAndUpdate(
+            { email },
+            { isActive: false },
+            { new: true }
+        );
+
+        if (updatedEmail) {
+            console.log(`Email ${email} ha sido desactivado.`);
+            res.status(200).send("Email desactivado exitosamente");
+        } else {
+            console.log(`Email ${email} no encontrado.`);
+            res.status(404).send("Email no encontrado");
+        }
+    } catch (error) {
+        console.error(`Error desactivando el email ${email}:`, error);
+        res.status(500).send("Error desactivando el email");
+    }
+});
+
+// Ruta para el webhook de Diamond usuario reactivado
+app.post("/updateEmailEnigmario", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send("Email es requerido");
+    }
+
+    try {
+        const updatedEmail = await UsedEmailEnigmario.findOneAndUpdate(
+            { email },
+            { isActive: true },
+            { new: true }
+        );
+
+        if (updatedEmail) {
+            console.log(`Email ${email} ha sido actualizado.`);
+            res.status(200).send("Email actualizado exitosamente");
+        } else {
+            console.log(`Email ${email} no encontrado.`);
+            res.status(404).send("Email no encontrado");
+        }
+    } catch (error) {
+        console.error(`Error actualizando el email ${email}:`, error);
+        res.status(500).send("Error actualizando el email");
+    }
+});
+
+
 
 app.post("/updateDatabase", async (req,res) => {
     const { email, telegramId } = req.body;
